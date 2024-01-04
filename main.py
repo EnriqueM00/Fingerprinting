7f3aabd012625c4b1b2cf5b47becd6ce2a3d7635
Javier Cabezas <jcabezasp@gmail.com>
2024-01-04 17:33:56 +0100

    Add IP variable and refactor code

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ for direction in directions:
                 print("IP: {} ".format(device['ip']))
                 add_line_to_txt_file("reporte.txt", "IP: {} ".format(device['ip']))
                 add_line_to_txt_file("reporte.txt", "MAC: {} ".format(device['mac']))
+                ip = device['ip']
                 host_name = get_hostname(ip)
                 if host_name is not None:
                     print("Nombre del host: " + host_name)
@@ -36,7 +37,6 @@ for direction in directions:
 
                 print("Escaneando servicios de la ip " + device['ip'])
             
-                ip = device['ip']
                 open_ports = scan_ports(ip)
                 if open_ports == "honeypot_detected":
                     print("Todos los puertos están abiertos. Esto podría ser un honeypot.")
